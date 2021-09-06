c947a0b0e822b8500dcecabe150b5ca8cca55a09
Steph Poleij <99033734@mydavinci.nl>
2021-09-06 23:31:37 +0200

    nette output

--- feestlunch.py
+++ feestlunch.py
@@ -5,4 +5,9 @@ prijsStokbroden = 2.78
 aantalKortingsbonnen = 3
 prijsKortingsbonnen = 0.50
 
-print ((aantalCroissantjes * prijsCroissantjes) + (aantalStokbroden * prijsStokbroden) - (aantalKortingsbonnen * prijsKortingsbonnen))
+totaalPrijs = ((aantalCroissantjes * prijsCroissantjes) + (aantalStokbroden * prijsStokbroden) - (aantalKortingsbonnen * prijsKortingsbonnen))
+
+#zorg voor dit antwoord: ‘De feestlunch kost je bij de bakker 10.69 euro voor de 17 croissantjes en de 2 stokbroden als de 3 kortingsbonnen nog geldig zijn!’
+
+print ('De feestlunch kost je bij de bakker ' + str(totaalPrijs) + ' euro' + ' voor de ' + str(aantalCroissantjes) +' croissantjes en de ' + str(aantalStokbroden)
++ ' stokbroden als de ' + str(aantalKortingsbonnen) + ' kortingsbonnen nog geldig zijn')
--- speelhaldag.py
+++ speelhaldag.py
@@ -4,4 +4,8 @@ tijdGameSeat = 45
 verhoudingGameSeat = 5
 prijsGameSeat = 0.37
 
-print ((aantalPersonen * prijsTicket) + (tijdGameSeat / verhoudingGameSeat * prijsGameSeat * aantalPersonen))
+totaalPrijs = ((aantalPersonen * prijsTicket) + (tijdGameSeat / verhoudingGameSeat * prijsGameSeat * aantalPersonen))
+
+# zorg voor dit antwoord: ‘Dit geweldige dagje-uit met 4 mensen in de Speelhal met 45 minuten VR kost je maar 44.44 euro’
+
+print ('Dit geweldige dagje-uit met ' +str(aantalPersonen) +' mensen in de Speelhal met ' + str(tijdGameSeat) + ' minuten VR kost je maar ' + str(round(totaalPrijs, 2)) + ' euro')
