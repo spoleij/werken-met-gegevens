44dbdff89b743a8235d4248821379e0fc6487a08
Steph Poleij <99033734@mydavinci.nl>
2021-09-06 22:33:16 +0200

    nieuw

---
+++ feestlunch.py
@@ -0,0 +1,8 @@
+aantalCroissantjes = 17
+prijsCroissantjes = 0.39
+aantalStokbroden = 2
+prijsStokbroden = 2.78
+aantalKortingsbonnen = 3
+prijsKortingsbonnen = 0.50
+
+print ((aantalCroissantjes * prijsCroissantjes) + (aantalStokbroden * prijsStokbroden) - (aantalKortingsbonnen * prijsKortingsbonnen))
---
+++ speelhaldag.py
@@ -0,0 +1,7 @@
+aantalPersonen = 4
+prijsTicket = 7.45
+tijdGameSeat = 45
+verhoudingGameSeat = 5
+prijsGameSeat = 0.37
+
+print ((aantalPersonen * prijsTicket) + (tijdGameSeat / verhoudingGameSeat * prijsGameSeat * aantalPersonen))
